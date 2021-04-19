0d0a757a2cbddff397df3290b21b18ffa6bc8fa3
TheUniT19 <63451458+TheUniT19@users.noreply.github.com>
2021-04-19 22:32:00 +0200

    Initial commit

---
+++ TimedShutdown/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+
+namespace TimedShutdown
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Time(object sender, RoutedEventArgs e)
+        {
+            var success = DateTime.TryParse(Input.Text, out var time);
+            if (success)
+            {
+                Process.Start("cmd.exe", "/C shutdown -a");
+                if (DateTime.Now > time)
+                {
+                    time = time.AddDays(1);
+                }
+                var totalSeconds = (float)(time - DateTime.Now).TotalSeconds;
+                var roundedSeconds = totalSeconds.ToString("#");
+                Process.Start("cmd.exe", $"/C shutdown -s -t {roundedSeconds}");
+            }
+            else
+            {
+                success = float.TryParse(Input.Text, out var hours);
+                if (success)
+                {
+                    Process.Start("cmd.exe", "/C shutdown -a");
+                    var timespan = TimeSpan.FromHours(hours);
+                    Process.Start("cmd.exe", $"/C shutdown -s -t {timespan.TotalSeconds}");
+                }
+            }
+        }
+        private void Cancel(object sender, RoutedEventArgs e)
+        {
+            Process.Start("cmd.exe", "/C shutdown -a");
+        }
+    }
+}
