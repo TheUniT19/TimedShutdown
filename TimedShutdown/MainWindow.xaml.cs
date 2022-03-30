b155667bd9661587efcf5a5fcdebc91bedc72f77
TheUniT19 <63451458+TheUniT19@users.noreply.github.com>
2022-03-30 20:51:13 +0200

    It's nicer looking and you can cancel shit now

--- TimedShutdown/MainWindow.xaml.cs
+++ TimedShutdown/MainWindow.xaml.cs
@@ -1,55 +1,31 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Windows;
 using System.Windows.Threading;
 
 namespace TimedShutdown
 {
-    /// <summary>
-    /// Interaktionslogik für MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        public class Cancellation
+        {            
+            public bool IsCancelled { get; set; }
+        }
+        public List<Cancellation> Cancellations { get; set; } = new List<Cancellation>();
         public MainWindow()
         {
             InitializeComponent();
         }
-        private void Time(object sender, RoutedEventArgs e)
-        {
-            var success = float.TryParse(Input.Text, out var hours);
-            if (success)
-            {
-                Process.Start("cmd.exe", "/C shutdown -a");
-                var timespan = TimeSpan.FromHours(hours);
-
-                DelayAction(500, new Action(() => 
-                {
-                    Process.Start("cmd.exe", $"/C shutdown -s -t {timespan.TotalSeconds}");
-                }));
-            }
-
-            else
-            {
-                success = DateTime.TryParse(Input.Text, out var time);
-                if (success)
-                {
-                    Process.Start("cmd.exe", "/C shutdown -a");
-                    if (DateTime.Now > time)
-                    {
-                        time = time.AddDays(1);
-                    }
-                    var totalSeconds = (float)(time - DateTime.Now).TotalSeconds;
-                    var roundedSeconds = totalSeconds.ToString("#");
-                    DelayAction(500, new Action(() =>
-                    {
-                        Process.Start("cmd.exe", $"/C shutdown -s -t {roundedSeconds}");
-                    }));
-                }
-            }
-        }
         private void Cancel(object sender, RoutedEventArgs e)
         {
+            CancelAll();
+        }
+
+        private void CancelAll()
+        {
             Process.Start("cmd.exe", "/C shutdown -a");
+            Cancellations.ForEach(x => x.IsCancelled = true);
         }
 
         private static void DelayAction(int millisecond, Action action)
@@ -64,6 +40,82 @@ namespace TimedShutdown
 
             timer.Interval = TimeSpan.FromMilliseconds(millisecond);
             timer.Start();
+        }
+
+        private void Shutdown(object sender, RoutedEventArgs e)
+        {
+            if (float.TryParse(Input.Text, out var hours))
+            {
+                CancelAll();
+                var timespan = TimeSpan.FromHours(hours);
+
+                DelayAction(500, new Action(() =>
+                {
+                    Process.Start("cmd.exe", $"/C shutdown -s -t {timespan.TotalSeconds}");
+                }));
+            }
+
+            else
+            {
+                if (DateTime.TryParse(Input.Text, out var time))
+                {
+                    CancelAll();
+                    if (DateTime.Now > time)
+                    {
+                        time = time.AddDays(1);
+                    }
+                    var totalSeconds = (float)(time - DateTime.Now).TotalSeconds;
+                    var roundedSeconds = totalSeconds.ToString("#");
+                    DelayAction(500, new Action(() =>
+                    {
+                        Process.Start("cmd.exe", $"/C shutdown -s -t {roundedSeconds}");
+                    }));
+                }
+            }
+        }
+
+        private void Hibernate(object sender, RoutedEventArgs e)
+        {
+            if (float.TryParse(Input.Text, out var hours))
+            {
+                CancelAll();
+                var timespan = TimeSpan.FromHours(hours);
+                var cancellation = new Cancellation();
+                Cancellations.Add(cancellation);
+
+                DelayAction((int)timespan.TotalMilliseconds, new Action(() =>
+                {
+                    if (!cancellation.IsCancelled)
+                    {
+                        Process.Start("cmd.exe", $"/C shutdown -h");
+                    }
+                    Cancellations.Remove(cancellation);
+                }));
+            }
+
+            else
+            {
+                if (DateTime.TryParse(Input.Text, out var time))
+                {
+                    CancelAll();
+                    if (DateTime.Now > time)
+                    {
+                        time = time.AddDays(1);
+                    }
+                    var totalMs = (int)(time - DateTime.Now).TotalMilliseconds;
+                    var cancellation = new Cancellation();
+                    Cancellations.Add(cancellation);
+
+                    DelayAction(totalMs, new Action(() =>
+                    {
+                        if (!cancellation.IsCancelled)
+                        {
+                            Process.Start("cmd.exe", $"/C shutdown -h");
+                        }
+                        Cancellations.Remove(cancellation);
+                    }));
+                }
+            }
         }
     }
 }
